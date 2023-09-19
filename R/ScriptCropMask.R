5996ceef6ba6c8f920032bee6b31cf464249e42a
FagnerF <fagnerengenheiroambiental@gmail.com>
2023-09-19 15:11:08 -0300

    Updated

--- R/ScriptCropMask.R
+++ R/ScriptCropMask.R
@@ -1,6 +1,7 @@
 #' This package performs cropping of raster files
 #'
 #' @param Directory Location where the files will be saved
+#' @param shapefile shapefile local
 #'
 #' @importFrom stats na.omit
 #' @importFrom magrittr %>%
@@ -11,53 +12,108 @@
 #' @importFrom raster raster crs extent mask crop writeRaster
 #' @import filesstrings
 #' @importFrom geobr read_municipality
+#' @importFrom rgdal readOGR
 #'
 #' @export
-ScriptCropMask <- function(Directory) {
-  pathNC <- file.choose()
+ScriptCropMask <- function(shapefile, Directory) {
+  if (shapefile == "0") {
+    pathNC <- file.choose()
 
-  nc <- raster::raster(pathNC)
+    nc <- raster::raster(pathNC)
 
-  raster::crs(nc) <-
-    CRS("+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs")
+    raster::crs(nc) <-
+      CRS("+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs")
 
-  pathCondMun <- file.choose()
+    pathCondMun <- file.choose()
 
-  CondMun <- list()
+    CondMun <- list()
 
-  CondMun <- readxl::read_xlsx(pathCondMun) %>%
-    stats::na.omit()
+    CondMun <- readxl::read_xlsx(pathCondMun) %>%
+      stats::na.omit()
 
-  for (i in 1:length(CondMun$Municipio)) {
-    cat("== Working on the data ==", "\n")
-    cat("Wait...", "\n")
-    cat(paste0("Crop from", " ", CondMun$Municipio[i], "\n"))
+    for (i in 1:length(CondMun$Municipio)) {
+      cat("== Working on the data ==", "\n")
+      cat("Wait...", "\n")
+      cat(paste0("Crop from", " ", CondMun$Municipio[i], "\n"))
+
+      shp <- list()
+      MaskCrop <- list()
+
+      shp <-
+        geobr::read_municipality(code_muni =  CondMun$Codigo[i], year = 2020)
+
+      # crop and mask
+      MaskCrop <-
+        raster::mask(raster::crop(nc, raster::extent(shp)), shp)
+
+      # export para ".tif"
+      raster::writeRaster(
+        MaskCrop,
+        paste0(Directory, "/Exit/" , "MasCrop_",  CondMun$Codigo[i]),
+        format = "GTiff",
+        bylayer = TRUE,
+        options = c("COMPRESS=NONE", "TFW=YES"),
+        overwrite = TRUE
+
+
+      )
+
+      cat("\014")
+
+    }
+
+  } else if (shapefile == "1") {
+    all_files  <- list.files(path=paste0(Directory, "/Rasters"),
+                         full.names = TRUE,
+                         pattern = ".tif$")
+
+    name_files  <- list.files(path=paste0(Directory, "/Rasters"),
+                             full.names = FALSE,
+                             pattern = ".tif$")
 
     shp <- list()
-    MaskCrop <- list()
 
-    shp <-
-      geobr::read_municipality(code_muni =  CondMun$Codigo[i], year = 2020)
+    shp <- list.files(path=paste0(Directory, "/BaseMap"),
+                                full.names = TRUE,
+                                pattern = ".shp")
 
-    # crop and mask
-    MaskCrop <-
-      raster::mask(raster::crop(nc, raster::extent(shp)), shp)
+    shp <- rgdal::readOGR(dsn=shp, verbose = FALSE,
+                          stringsAsFactors=FALSE)
 
-    # export para ".tif"
-    raster::writeRaster(
-      MaskCrop,
-      paste0(Directory, "/" , "MasCrop_",  CondMun$Codigo[i]),
-      format = "GTiff",
-      bylayer = TRUE,
-      options = c("COMPRESS=NONE", "TFW=YES"),
-      overwrite = TRUE
+    for (i in 1:length(all_files)) {
+      cat("== Working on the data ==", "\n")
+      cat("Wait...", "\n")
+      cat(paste0("Crop from", " ", all_files[i],"\n"))
+
+      MaskCrop <- list()
+
+      nc <- raster::raster(all_files[i])
+
+      raster::crs(nc) <-
+      CRS("+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs")
+
+      # crop and mask
+      MaskCrop <-
+        raster::mask(raster::crop(nc, raster::extent(shp)), shp)
+
+      # export para ".tif"
+      raster::writeRaster(
+        MaskCrop,
+        paste0(Directory, "/Exit/" , "MasCrop_", name_files[i]),
+        format = "GTiff",
+        bylayer = TRUE,
+        options = c("COMPRESS=NONE", "TFW=YES"),
+        overwrite = TRUE
 
 
-    )
+      )
 
-    cat("\014")
+      cat("\014")
+
+    }
 
   }
+
 
   return(cat("Process finished!", "\n"))
 
